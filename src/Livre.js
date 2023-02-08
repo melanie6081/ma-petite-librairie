5bbb6c93f5d5403dd355eb4c9db8f55085b83d55
melanie <melanie.feron@etud.univ-jfc.fr>
2023-02-08 18:47:25 +0000

    Update 21 files

---
+++ src/Livre.js
@@ -0,0 +1,22 @@
+// Un livre
+export default class Livre {
+  constructor(titre, qtestock, prix) {
+    this._titre = titre;
+    this._qtestock = qtestock;
+    this._prix = prix;
+  }
+
+  get titre() {
+    return this._titre;
+  }
+  get qtestock() {
+    return this._qtestock;
+  }
+  get prix() {
+    return this._prix;
+  }
+
+  pourAfficher() {
+    return `--> ${this._titre} (${this._qtestock} en stock | ${this._prix}€)`;
+  }
+}
