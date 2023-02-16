579ef5260015cab4b5cb261877a5e61632794aea
melanie <melanie.feron@etud.univ-jfc.fr>
2023-02-16 08:54:46 +0000

    Update 3 files

--- src/components/LibrairieHeader.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>Ma Petite Librairie</h1>
-</template>
-
-<style scoped>
-h1 {
-  margin: 40px 0 0;
-}
-</style>
--- src/views/LibrairieView.vue
+++ src/views/LibrairieView.vue
@@ -1,23 +1,17 @@
 <script setup>
-import LibrairieHeader from "../components/LibrairieHeader.vue";
 import Librairie from "../components/Librairie.vue";
 </script>
 
 <template>
   <main>
-    <div>
-      <h1><LibrairieHeader /></h1>
-    </div>
-    <div>
+    <div id="librairie">
       <Librairie />
     </div>
   </main>
 </template>
 
 <style scoped>
-h1 {
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  font-size: xx-large;
-  font-style: oblique;
+#librairie {
+  margin: 160px 0px 0px;
 }
 </style>
